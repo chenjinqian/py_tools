85154ff3980b10dc6808c589e7f0895408fa3347
jinqian.chen <chenjinqian@horizonenergy.com.cn>
2016-12-19 16:20:56 +0800

    fixed bugs in  ReadConfig_DB.

--- read_config.py
+++ read_config.py
@@ -13,11 +13,18 @@ from read_config import ReadConfig as ReadConfig
 cfg = ReadConfig(path),
 cfg.config to get the cached config dic.
 cfg_dic=cfg.check_config() to get a refreshed config,
+
+DB type config file is like:
+[mysql:db_name]
+ip=xx.xx.xx.xx
+port = xx
+
+use ReadConfig_DB will read this file in dictory version.
 """
 
 import re, os, sys
 
-__all__ = ['ReadConfig', 'ReadConfig2']
+__all__ = ['ReadConfig', 'ReadConfig_DB']
 
 
 class ReadConfig(object):
@@ -26,7 +33,6 @@ class ReadConfig(object):
         self.fpath=str(fpath)
         self.config={}
         self.check_config()
-
 
     def check_config(self):
         config = {}
@@ -60,41 +66,47 @@ class ReadConfig(object):
         return config
 
 
-
 class ReadConfig_DB(object):
     def __init__(self, fpath, convert_port=True):
         "docstring"
-        self.path = str(fpath)
+        self.fpath = str(fpath)
         self.convert_port = convert_port
         self.check_config()
 
-    def check_config(self, convert_port=convert_port):
+    def check_config(self):
         try:
             with open(self.fpath, 'rb') as f:
                 self.data = f.readlines()
         except:
             self.config = {}
             return self.config
-        zone_name = ''
+        zone_key = ''
         config = {}
         for d in self.data:
+            # print(d)
             d = d.replace('\40', '')
             d = d.replace('\r', '')
             d = d.replace('\n', '')
             if d=='' or d[0] == '#':
                 continue
             re_m = re.match('^\[(.*)\]$', d)
+            # print(d, re_m)
             if re_m:
-                zone_name= re_m.group(1)
-                config[zone_name] = {}
-                print(zone_name)
+                zone_key= re_m.group(1)
+                config[zone_key] = {}
+                # print(zone_key)
             elif '=' in d:
                 d = d.split('=')
                 if not len(d) == 2:
                     continue
                 if zone_key:
-                    if convert_port and d[0] in ['port', 'Port', 'PORT']:
-                        config[zone_key][d[0]] = int(d[1])
+                    if self.convert_port and d[0] in ['port', 'Port', 'PORT']:
+                        try:
+                            config[zone_key][d[0]] = int(d[1])
+                        except:
+                            print('fail in converting port to int')
+                            return -1
+                            # if fail to convert, just crash print.
                     else:
                         config[zone_key][d[0]] = d[1]
             else:
@@ -103,19 +115,22 @@ class ReadConfig_DB(object):
         return self.config
 
 
-
-
 def main():
-    # TODO: If args is -h, then print help message.
-    print("tool class for reading config")
-    print("Usage: cfg = ReadConfig(path), cfg_dic=cfg.check_config() to get a refreshed config, and cfg.config to get the cached config dic.")
-    print("reading config file db.info... ")
-    import os
-    fp = os.path.realpath('db.info')
-    config=ReadConfig(fp)
-    # config_dic = config.check_config()
-    config_dic = config.config
-    print("config_dic is %s" % config_dic)
+    # # TODO: If args is -h, then print help message.
+    # print("tool class for reading config")
+    # print("Usage: cfg = ReadConfig(path), cfg_dic=cfg.check_config() to get a refreshed config, and cfg.config to get the cached config dic.")
+    # print("reading config file db.info... ")
+    # import os
+    # fp = os.path.realpath('db.info')
+    # config=ReadConfig(fp)
+    # # config_dic = config.check_config()
+    # config_dic = config.config
+    # print("config_dic is %s" % (config_dic))
+    fp2 = os.path.realpath('./db.ini')
+    config2 = ReadConfig_DB(fp2)
+    config_dic2 = config2.config
+    config_dic2_checkconfig = config2.check_config()
+    print("config_dic2 is %s" % (config_dic2))
 
 
 if __name__ == '__main__':
