33010521664151d9516d50153cc0539b6e8b7c61
jinqian.chen <chenjinqian@horizonenergy.com.cn>
2017-01-06 09:17:27 +0800

    writing .

--- bin/app_calc.py
+++ bin/app_calc.py
@@ -37,34 +37,54 @@ def mk_rp_d(ini='../config/db.ini', mark='redis:'):
 
 # use dictory for global database connection pool instance.
 cfgd = rcfg.ReadConfig_DB('../config/db.ini').check_config(db_type='mysql', convert_port=True)
-my_pol_d, my_pol_w = mk_mp_d()
+my_pol_n, my_pol_w = mk_mp_d()
 rd_pol_c = mk_rp_d()
 
 
-def test_func_prop():
-    print(my_pool1.pool.cnx_now)
-    con1 = my_pool1.pool.getConnection()
-    print(my_pool1.pool.cnx_now)
-    return con1
+# def test_func_prop():
+#     print(my_pol_d['mysql:app_eemsyd'].pool.cnx_now)
+#     con1 = my_pol_d['mysql:app_eemsyd'].pool.getConnection()
+#     print(my_pol_d['mysql:app_eemsyd'].pool.cnx_now)
+#     return con1
 
 
+def calc_main(ha=None, hb=None, mid='', app='eemsyd'):
+    if not ha or not hb:
+        ha, hb = calc_acc(pre=True)
+    a, b = calc_acc(ha, hb, mid=mid)
+    sql_op(ha, a, hb, b, mid=mid, app=app)
+    how_sleep()
+    calc_main(a, b, mid=mid, app=app)
 
-def calc_acc(ha=None, hb=None, pre=False, meter_id=''):
+
+def calc_acc(ha=None, hb=None, pre=False, mid='2017'):
     key_lst = mk_tm_key(merter_id, pre=pre)
     a, b = interv(ha, hb)
     return [a, b]
 
 
-def calc_main(ha=None, hb=None, meter_id=''):
-    if not ha or not hb:
-        ha, hb = calc_acc(pre=True)
-    a, b = calc_acc(ha, hb, meter_id=meter_id)
-    sql_op(ha, a, hb, b)
-    time.sleep(60 * 15)
-    calc_main(a, b, meter_id=meter_id)
+def sql_op(ha, a, hb, b, mid='2017', app='eemsyd'):
+    dif_a = ha - a
+    dif_b = hb - b
+    def mksql(app, kwh=None, pttl=None):
+        s = 'select * from %s' % (app)
+        reutrn s
+    def mksql_last_15_min(mid='2', point=3, table='', time_end='', simple_lst=True):
+        import time
+        table = '' or 'elec_company_15min_%s' % time.strftime('%Y', time.localtime())
+        example_sql = 'select * from (select * from elec_company_15min_2016 where company_id = 2  and stat_time < "2016-12-23 17:45:00" order by stat_time desc, stat_time limit 9600) t order by stat_time;'
+        time_end = time_end or fm_tm()
+        sql = 'select * from (select * from %s where company_id = %s  and stat_time < "%s" order by stat_time desc, stat_time limit %s) t order by stat_time;' % (table, comp, time_end, point)
+        if simple_lst:
+            sql = 'select stat_time, kwh from (select * from %s where company_id = %s  and stat_time < "%s" order by stat_time desc, stat_time limit %s) t order by stat_time;' % (table, comp, time_end, point)
+        return sql
 
 
-
+def how_sleep():
+    import time
+    sleep_for = 60 * 15
+    time.sleep(sleep_for)
+    return sleep_for
 
 
 
